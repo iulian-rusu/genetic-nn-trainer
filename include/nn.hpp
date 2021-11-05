9ebdf2310324a9aaf033f638e25d74f4af83ad99
Iulian <iulian.rusu999@gmail.com>
2021-11-05 13:19:35 +0200

    Update CMakeLists.txt

--- include/nn.hpp
+++ include/nn.hpp
@@ -1,9 +1,9 @@
-#ifndef GENETIC_NN_TRAINER_NN_HPP
-#define GENETIC_NN_TRAINER_NN_HPP
+#ifndef GNNT_NN_HPP
+#define GNNT_NN_HPP
 
 constexpr int get_magic_number()
 {
     return 69;
 }
 
-#endif //GENETIC_NN_TRAINER_NN_HPP
+#endif //GNNT_NN_HPP
